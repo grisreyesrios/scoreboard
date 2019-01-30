3c99b9d984320a555522f155b841c78a0418d9b1
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2019-01-29 18:47:14 -0600

    Adding index.html, src/App.css, src/App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,51 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            const Header = () => {
+              return (
+   <header>
+    <h1>Scoreboard</h1>
+    <span className="stats">Players: 1</span>
+   </header>
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Grisell
+      </span>
+      <Counter />
+    </div>  
+  );
+}
+
+const Counter = () => {
+  return (
+    <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">40</span>
+        <button className="counter-action increment"> + </button>
+     </div>  
+  );
+}
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+      
+     
+      <Player />    
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
           </p>
           <a
             className="App-link"
